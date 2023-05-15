fc8924799612df24c4bda39dfe051f893e0086b9
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-15 11:10:36 +0200

    Suite exercice guidé, formulaires d'ajout

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class EmployeController extends AbstractController
 {
@@ -17,6 +19,28 @@ class EmployeController extends AbstractController
         $employes = $entityManager->getRepository(Employe::class)->findBy([], ["nom" => "ASC"]);
         return $this->render('employe/index.html.twig', [
             'employes' => $employes
+        ]);
+    }
+
+    #[Route('/employe/add', name: 'add_employe')]
+    public function add(EntityManagerInterface $entityManager, Employe $employe = null, Request $request): Response{
+        
+        $form = $this->createForm(EmployeType::class, $employe);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $employe = $form->getData();
+            // prepare
+            $entityManager->persist($employe);
+            // insert into (execute)
+            $entityManager->flush();
+
+            return $this->redirectToRoute("app_employe");
+        }
+
+        // vue pour afficher le formulaire d'ajout
+        return $this->render('employe/add.html.twig', [
+            "formAddEmploye" => $form->createView()
         ]);
     }
 
